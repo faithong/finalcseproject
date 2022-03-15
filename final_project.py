6963519d4e1ee98f44c2e7e9649c2d6b485e1c20
faithong <faithong02@gmail.com>
2022-03-15 12:39:02 -0700

    updates

--- final_project.py
+++ final_project.py
@@ -1,6 +1,9 @@
 import streamlit as st
 import pandas as pd
 import seaborn as sns
+import matplotlib.pyplot as plt
+from collections import Counter
+import matplotlib.pyplot as plt
 
 
 col1, col2, col3 = st.columns([3, 1, 1])
@@ -31,24 +34,116 @@ st.markdown("***")
 st.text("")
 
 if visualization == 'How Does the Average APA Site in African Americans Compare to that of European Americans?':
-     print("apa")
+    st.subheader("Poly A Sites Mean")
+    poly_a_mean = all_races.groupby('Race')['polyA_sites_in_gene'].mean()
+    poly_a_mean
+    st.subheader("Poly A Sites Sum")
+    poly_a_sum = all_races.groupby('Race')['polyA_sites_in_gene'].sum()
+    poly_a_sum
+    st.subheader("Poly A Sites Median")
+    poly_a_median = all_races.groupby('Race')['polyA_sites_in_gene'].median()
+    poly_a_median
 
 elif visualization == 'How does the average PSI index in African Americans compare to that of European Americans?':
-    psisheet = pd.read_csv('/home/psi_sheet.csv')
+    psisheet = pd.read_csv('psi_sheet.csv')
     psisheet
     psisheet['psi_mean'] = psisheet.mean(axis=1)
     all_races['gene_name_class'] = all_races['gene_name'] + all_races['gene_class']
     joined_sheets = all_races.merge(psisheet, how="inner")
-    race_psi_index = joined_sheets.groupby('Race')['psi_mean'].mean()
-    race_psi_index
-    race_psi_index = joined_sheets.groupby('Race')['psi_mean'].median()
-    race_psi_index
-    race_psi_index = joined_sheets.groupby('Race')['psi_mean'].sum()
-    race_psi_index
+    col4, col5, col6 = st.columns([3, 3, 3])
+    with col4:
+        race_psi_index = joined_sheets.groupby('Race')['psi_mean'].mean()
+        race_psi_index
+    with col5:
+        race_psi_index = joined_sheets.groupby('Race')['psi_mean'].median()
+        race_psi_index
+    with col6:
+        race_psi_index = joined_sheets.groupby('Race')['psi_mean'].sum()
+        race_psi_index
 elif visualization == 'Are there any commonalities between amino acid sequences among African American vs. European Americans?':
-    print("commonalites")
+    with open('gene_sequences_aa.txt') as f: 
+        gene_aa = Counter(c for line in f for c in line.strip())
+    with open('gene_sequences_ea.txt') as f: 
+        gene_ea = Counter(c for line in f for c in line.strip())
+    diff = Counter(gene_aa) - Counter(gene_ea)
+
+    # aa
+    lists = sorted(gene_aa.items())
+    x, y = zip(*lists)
+    plt.bar(x, y)
+    plt.title('Amino Acids and Codons in African Americans')
+    plt.xlabel('Amino Acids (Upper Case) & Codons (lower case)')
+    plt.ylabel('Count (n)')
+    st.pyplot(fig=plt)
+
+    # ae
+    lists = sorted(gene_ea.items())
+    x, y = zip(*lists)
+    plt.bar(x, y)
+    plt.title('Amino Acids and Codons in European Americans')
+    plt.xlabel('Amino Acids (Upper Case) & Codons (lower case)')
+    plt.ylabel('Count (n)')
+    plt.savefig('gene_aa.png')
+    st.pyplot(fig=plt)
+
+    # both
+    lists = sorted(diff.items())
+    x, y = zip(*lists)
+    plt.bar(x, y)
+    plt.title('Amino Acids and Codons Differences in African and European Americans')
+    plt.xlabel('Amino Acids (Upper Case) & Codons (lower case)')
+    plt.ylabel('Count (n)')
+    st.pyplot(fig=plt)
+
 elif visualization == 'Length of Amino Acids':
-    print("length")
+    
+    # length stuff for ea
+    st.header("Length Observations for European American Amino Acids")
+    with open ('gene_sequences_ea.txt') as f: 
+        count = 0
+        total = 0
+        min_amino_acid = 0
+        max_amino_acid = 0
+        lines = f.readlines()
+        for line in lines:
+            count += 1
+            total += len(line)
+            if min_amino_acid == 0:
+                min_amino_acid = len(line)
+            elif len(line) < min_amino_acid:
+                min_amino_acid = len(line)
+            if len(line) > max_amino_acid:
+                max_amino_acid = len(line)
+        st.subheader("Mean Length:")
+        total / count
+        st.subheader("Minimum Length:")
+        min_amino_acid
+        st.subheader("Maximum Length:")
+        max_amino_acid
+    
+    st.header("Length Observations for European American Amino Acids")
+    with open ('gene_sequences_aa.txt') as f: 
+        count = 0
+        total = 0
+        min_amino_acid = 0
+        max_amino_acid = 0
+        lines = f.readlines()
+        for line in lines:
+            count += 1
+            total += len(line)
+            if min_amino_acid == 0:
+                min_amino_acid = len(line)
+            elif len(line) < min_amino_acid:
+                min_amino_acid = len(line)
+            if len(line) > max_amino_acid:
+                max_amino_acid = len(line)
+        st.subheader("Mean Length:")
+        total / count
+        st.subheader("Minimum Length:")
+        min_amino_acid
+        st.subheader("Maximum Length:")
+        max_amino_acid
+
 elif visualization == 'About the Dataset':
     st.subheader("Dataset")
     all_races
