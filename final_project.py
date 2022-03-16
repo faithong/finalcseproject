5607d6f8af2dec186c99b7d121a52164f662774f
faithong <faithong02@gmail.com>
2022-03-15 19:13:19 -0700

    changes

--- final_project.py
+++ final_project.py
@@ -10,7 +10,11 @@ col1, col2, col3 = st.columns([3, 1, 1])
 
 with col1:
     st.title("CSE 163 Final Project")
-    st.caption("There are social and economic factors that influence the rate of lung cancer in different communities. However, despite racial disparities in lung cancer, genomic level analysis of lung cancer in underserved patients of African descent remains underexplored.")
+    st.caption("There are social and economic factors that \
+                influence the rate of lung cancer in different communities.\
+                However, despite racial disparities in lung cancer, genomic\
+                level analysis of lung cancer in underserved patients of\
+                African descent remains underexplored.")
 
 # displaying data
 aa = pd.read_csv('data_aa.csv')
@@ -23,15 +27,19 @@ all_races = all_races.loc[:, all_races.columns != 'gene_id']
 visualization = st.selectbox(
      "",
      ('About the Dataset',
-      'How Does the Average APA Site in African Americans Compare to that of European Americans?',
-      'How does the average PSI index in African Americans compare to that of European Americans?',
-      'Are there any commonalities between amino acid sequences among African American vs. European Americans?',
+      'How Does the Average APA Site in African Americans\
+       Compare to that of European Americans?',
+      'How does the average PSI index in African Americans\
+       compare to that of European Americans?',
+      'Are there any commonalities between amino acid\
+       sequences among African American vs. European Americans?',
       'Length of Amino Acids'))
 
 st.markdown("***")
 st.text("")
 
-if visualization == 'How Does the Average APA Site in African Americans Compare to that of European Americans?':
+if visualization == 'How Does the Average APA Site in African\
+                     Americans Compare to that of European Americans?':
     st.subheader("Poly A Sites Mean")
     poly_a_mean = all_races.groupby('Race')['polyA_sites_in_gene'].mean()
     poly_a_mean
@@ -42,11 +50,13 @@ if visualization == 'How Does the Average APA Site in African Americans Compare 
     poly_a_median = all_races.groupby('Race')['polyA_sites_in_gene'].median()
     poly_a_median
 
-elif visualization == 'How does the average PSI index in African Americans compare to that of European Americans?':
+elif visualization == 'How does the average PSI index in African\
+                       Americans compare to that of European Americans?':
     psisheet = pd.read_csv('psi_sheet.csv')
     psisheet
     psisheet['psi_mean'] = psisheet.mean(axis=1)
-    all_races['gene_name_class'] = all_races['gene_name'] + all_races['gene_class']
+    all_races['gene_name_class'] = all_races['gene_name']
+    + all_races['gene_class']
     joined_sheets = all_races.merge(psisheet, how="inner")
     col4, col5, col6 = st.columns([3, 3, 3])
     with col4:
@@ -58,10 +68,12 @@ elif visualization == 'How does the average PSI index in African Americans compa
     with col6:
         race_psi_index = joined_sheets.groupby('Race')['psi_mean'].sum()
         race_psi_index
-elif visualization == 'Are there any commonalities between amino acid sequences among African American vs. European Americans?':
-    with open('gene_sequences_aa.txt') as f: 
+elif visualization == 'Are there any commonalities between amino acid\
+                       sequences among African American vs. European\
+                       Americans?':
+    with open('gene_sequences_aa.txt') as f:
         gene_aa = Counter(c for line in f for c in line.strip())
-    with open('gene_sequences_ea.txt') as f: 
+    with open('gene_sequences_ea.txt') as f:
         gene_ea = Counter(c for line in f for c in line.strip())
     diff = Counter(gene_aa) - Counter(gene_ea)
 
@@ -88,16 +100,16 @@ elif visualization == 'Are there any commonalities between amino acid sequences 
     lists = sorted(diff.items())
     x, y = zip(*lists)
     plt.bar(x, y)
-    plt.title('Amino Acids and Codons Differences in African and European Americans')
+    plt.title('Amino Acids and Codons Differences in African\
+               and European Americans')
     plt.xlabel('Amino Acids (Upper Case) & Codons (lower case)')
     plt.ylabel('Count (n)')
     st.pyplot(fig=plt)
 
 elif visualization == 'Length of Amino Acids':
-    
     # length stuff for ea
     st.header("Length Observations for European American Amino Acids")
-    with open ('gene_sequences_ea.txt') as f: 
+    with open('gene_sequences_ea.txt') as f:
         count = 0
         total = 0
         min_amino_acid = 0
@@ -118,9 +130,8 @@ elif visualization == 'Length of Amino Acids':
         min_amino_acid
         st.subheader("Maximum Length:")
         max_amino_acid
-    
     st.header("Length Observations for European American Amino Acids")
-    with open ('gene_sequences_aa.txt') as f: 
+    with open('gene_sequences_aa.txt') as f:
         count = 0
         total = 0
         min_amino_acid = 0
@@ -150,21 +161,27 @@ elif visualization == 'About the Dataset':
     with col4:
         # visualization for gene_biotype
         st.subheader("Gene Biotypes")
-        st.caption("Here I will include a short explanation of what this graph shows.")
+        st.caption("Here I will include a short explanation\
+                    of what this graph shows.")
         st.write()
-        gene_biotype_graph = sns.catplot(data=all_races, x="gene_biotype", kind="count", ci=None)
+        gene_biotype_graph = sns.catplot(data=all_races, x="gene_biotype",
+                                         kind="count", ci=None)
         st.pyplot(gene_biotype_graph)
 
     with col5:
         # visualization for pair type
         st.subheader("Pair Types")
-        st.caption("Here I will include a short explanation of what this graph shows.")
-        pair_type_graph = sns.catplot(data=all_races, x="pair_type", kind="count", ci=None)
+        st.caption("Here I will include a short explanation\
+                    of what this graph shows.")
+        pair_type_graph = sns.catplot(data=all_races, x="pair_type",
+                                      kind="count", ci=None)
         st.pyplot(pair_type_graph)
 
     with col6:
         # visualization for gene class
         st.subheader("Gene Classes")
-        st.caption("Here I will include a short explanation of what this graph shows.")
-        gene_class_graph = sns.catplot(data=all_races, x="gene_class", kind="count", ci=None)
+        st.caption("Here I will include a short explanation of\
+                    what this graph shows.")
+        gene_class_graph = sns.catplot(data=all_races, x="gene_class",
+                                       kind="count", ci=None)
         st.pyplot(gene_class_graph)
